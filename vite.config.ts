8af7754de11c86b859fe5faf026eea4a15a916ed
Ze-Zheng Wu <zezhengwu@proton.me>
2025-06-16 00:38:40 +0800

    feat: add PWA support with vite-plugin-pwa and configure caching options

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import { version } from "./node_modules/@echogarden/icu-segmentation-wasm/package.json";
 
 // https://vite.dev/config/
@@ -15,5 +16,14 @@ export default defineConfig({
       `https://fastly.jsdelivr.net/npm/@echogarden/icu-segmentation-wasm@${version}/wasm/icu-segmentation.wasm`,
     ),
   },
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        maximumFileSizeToCacheInBytes: 52_428_800,
+      },
+    }),
+  ],
 });
